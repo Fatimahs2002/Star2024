20ab340f848d0b40a6ee3cea3692f5e47dfc481a
Mohammad Rahal <rahalmohamad21@gmail.com>
2024-05-23 11:24:43 +0300

    chaning to dashbaord

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+
+
 
 function App() {
-  return( <div className="App"></div>);
+  return(
+     <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </Router>
+
+
+  </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Faq from "../components/Faq"
+import Header from "../components/Header"
+
+
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <Faq/>
+    </div>
+  )
+}
+
+export default Home
